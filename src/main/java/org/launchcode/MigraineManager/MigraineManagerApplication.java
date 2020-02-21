f5f88a17ac1d346bac984b29aea0420447e3ad1f
Abigail Wiggins <rebtaylor14@gmail.com>
2020-02-21 02:31:50 -0600

    Initial commit

---
+++ src/main/java/org/launchcode/MigraineManager/MigraineManagerApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.MigraineManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigraineManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigraineManagerApplication.class, args);
+	}
+
+}
