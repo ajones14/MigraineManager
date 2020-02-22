4b08e144e91677ac6e3268f31a32b85b10330e9c
Abigail Wiggins <rebtaylor14@gmail.com>
2020-02-22 00:18:08 -0600

    Migraine class added

--- src/main/java/org/launchcode/MigraineManager/models/Migraine.java
+++ src/main/java/org/launchcode/MigraineManager/models/Migraine.java
@@ -1,4 +1,79 @@
 package org.launchcode.MigraineManager.models;
 
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+@Entity
 public class Migraine {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    private LocalDate startTime;
+
+    private LocalDate endTime;
+
+    private ArrayList<String> symptoms;
+
+    @NotNull
+    private int userId;
+
+    public Migraine () {}
+
+    public Migraine(LocalDate startTime, LocalDate endTime, int userId, ArrayList<String> symptoms) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.userId = userId;
+        this.symptoms = symptoms;
+    }
+
+    public Migraine(LocalDate startTime, LocalDate endTime, int userId) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.userId = userId;
+    }
+
+    public int getId () {
+        return id;
+    }
+
+    public LocalDate getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDate startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDate getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDate endTime) {
+        this.endTime = endTime;
+    }
+
+    public ArrayList<String> getSymptoms() {
+        return symptoms;
+    }
+
+    public void setSymptoms(ArrayList<String> symptoms) {
+        this.symptoms = symptoms;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 }
