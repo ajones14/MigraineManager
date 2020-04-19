643404139c03b64321517417bbe4215edcaa269e
Abigail Wiggins <rebtaylor14@gmail.com>
2020-04-19 02:48:32 -0500

    Message form validation

--- src/main/java/org/launchcode/MigraineManager/controllers/WelcomeController.java
+++ src/main/java/org/launchcode/MigraineManager/controllers/WelcomeController.java
@@ -3,9 +3,13 @@ package org.launchcode.MigraineManager.controllers;
 import org.launchcode.MigraineManager.models.dto.FeedbackFormDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @RequestMapping("/welcome")
 @Controller
@@ -13,13 +17,22 @@ public class WelcomeController {
 
     @GetMapping
     public String displayWelcomePage(Model model) {
-        model.addAttribute(new FeedbackFormDTO());
+        if (!model.containsAttribute("feedbackFormDTO")) {
+            model.addAttribute(new FeedbackFormDTO());
+        }
         return "welcome";
     }
 
     @PostMapping
-    public String processMessageForm() {
-        return "redirect:/welcome";
+    public String processFeedbackForm(@Valid @ModelAttribute("feedbackFormDTO") FeedbackFormDTO feedbackFormDTO, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            model.addAttribute("org.launchcode.MigraineManager.models.dto.FeedbackFormDTO", errors.getAllErrors());
+            model.addAttribute("feedbackFormDTO", feedbackFormDTO);
+            return "welcome";
+        }
+        model.addAttribute(new FeedbackFormDTO());
+        model.addAttribute("success", true);
+        return "welcome";
     }
 
 }
