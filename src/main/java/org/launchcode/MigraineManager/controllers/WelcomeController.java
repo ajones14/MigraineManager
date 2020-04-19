8712ea2f9559c001193f360ddcdee559ab1e4baa
Abigail Wiggins <rebtaylor14@gmail.com>
2020-04-19 05:51:36 -0500

    Feedback form sends emails to admin account

--- src/main/java/org/launchcode/MigraineManager/controllers/WelcomeController.java
+++ src/main/java/org/launchcode/MigraineManager/controllers/WelcomeController.java
@@ -1,6 +1,8 @@
 package org.launchcode.MigraineManager.controllers;
 
+import org.launchcode.MigraineManager.models.Email.EmailService;
 import org.launchcode.MigraineManager.models.dto.FeedbackFormDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -14,6 +16,9 @@ import javax.validation.Valid;
 @RequestMapping("/welcome")
 @Controller
 public class WelcomeController {
+
+    @Autowired
+    private EmailService emailService;
 
     @GetMapping
     public String displayWelcomePage(Model model) {
@@ -30,6 +35,8 @@ public class WelcomeController {
             model.addAttribute("feedbackFormDTO", feedbackFormDTO);
             return "welcome";
         }
+        emailService.sendMail("mymigrainemanager@gmail.com", "Feedback", "Feedback from: " + feedbackFormDTO.getName()
+                + " " + feedbackFormDTO.getEmail() + "\n" + feedbackFormDTO.getMessage());
         model.addAttribute(new FeedbackFormDTO());
         model.addAttribute("success", true);
         return "welcome";
