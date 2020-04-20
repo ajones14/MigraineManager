bbccfd486747cb25a5953191a35d9627dc74ab0e
Abigail Wiggins <rebtaylor14@gmail.com>
2020-04-20 02:14:13 -0500

    Empty migraine stats bug fixed

--- src/main/java/org/launchcode/MigraineManager/controllers/StatsController.java
+++ src/main/java/org/launchcode/MigraineManager/controllers/StatsController.java
@@ -66,30 +66,32 @@ public class StatsController {
         model.addAttribute("symptomData", symptomData);
 
         List<Migraine> migraineList = migraineRepository.findAllByUserId(currentUser.getId());
-        if (migraineList.get(migraineList.size() - 1).getEndTime() == null) {
-            migraineList.remove(migraineList.size() - 1);
-        }
-        migraineList.sort(Comparator.comparing(Migraine::getStartTime));
-        HashMap<String, Integer> migraineDaysPerMonth = new LinkedHashMap<>();
-        HashMap<String, Integer> migraineData = new LinkedHashMap<>();
-        for (Migraine migraine : migraineList) {
-            long longDays = DAYS.between(migraine.getStartTime().toLocalDate(), migraine.getEndTime().toLocalDate()) + 1;
-            Integer daysBetween = Math.toIntExact(longDays);
-            if (migraineData.containsKey(migraine.getStartTime().format(DateTimeFormatter.ofPattern("MMMM uuuu")))) {
-                int count = migraineData.get(migraine.getStartTime().format(DateTimeFormatter.ofPattern("MMMM uuuu")));
-                migraineData.replace(migraine.getStartTime().format(DateTimeFormatter.ofPattern("MMMM uuuu")), count + 1);
-
-                Integer currentDays = migraineDaysPerMonth.get(migraine.getStartTime().format(DateTimeFormatter.ofPattern("MMMM uuuu")));
-                migraineDaysPerMonth.replace(migraine.getStartTime().format(DateTimeFormatter.ofPattern("MMMM uuuu")), currentDays + daysBetween);
-            } else {
-                migraineData.put(migraine.getStartTime().format(DateTimeFormatter.ofPattern("MMMM uuuu")), 1);
-                migraineDaysPerMonth.put(migraine.getStartTime().format(DateTimeFormatter.ofPattern("MMMM uuuu")), daysBetween);
+        if (!migraineList.isEmpty()) {
+            System.out.println(migraineList.toString());
+            if (migraineList.get(migraineList.size() - 1).getEndTime() == null) {
+                migraineList.remove(migraineList.size() - 1);
             }
-        }
+            migraineList.sort(Comparator.comparing(Migraine::getStartTime));
+            HashMap<String, Integer> migraineDaysPerMonth = new LinkedHashMap<>();
+            HashMap<String, Integer> migraineData = new LinkedHashMap<>();
+            for (Migraine migraine : migraineList) {
+                long longDays = DAYS.between(migraine.getStartTime().toLocalDate(), migraine.getEndTime().toLocalDate()) + 1;
+                Integer daysBetween = Math.toIntExact(longDays);
+                if (migraineData.containsKey(migraine.getStartTime().format(DateTimeFormatter.ofPattern("MMMM uuuu")))) {
+                    int count = migraineData.get(migraine.getStartTime().format(DateTimeFormatter.ofPattern("MMMM uuuu")));
+                    migraineData.replace(migraine.getStartTime().format(DateTimeFormatter.ofPattern("MMMM uuuu")), count + 1);
 
-        model.addAttribute("migraineDaysPerMonth", migraineDaysPerMonth.values());
-        model.addAttribute("migraineLabels", migraineData.keySet());
-        model.addAttribute("migraineFrequency", migraineData.values());
+                    Integer currentDays = migraineDaysPerMonth.get(migraine.getStartTime().format(DateTimeFormatter.ofPattern("MMMM uuuu")));
+                    migraineDaysPerMonth.replace(migraine.getStartTime().format(DateTimeFormatter.ofPattern("MMMM uuuu")), currentDays + daysBetween);
+                } else {
+                    migraineData.put(migraine.getStartTime().format(DateTimeFormatter.ofPattern("MMMM uuuu")), 1);
+                    migraineDaysPerMonth.put(migraine.getStartTime().format(DateTimeFormatter.ofPattern("MMMM uuuu")), daysBetween);
+                }
+            }
+            model.addAttribute("migraineDaysPerMonth", migraineDaysPerMonth.values());
+            model.addAttribute("migraineLabels", migraineData.keySet());
+            model.addAttribute("migraineFrequency", migraineData.values());
+        }
 
         return "main/stats";
     }
